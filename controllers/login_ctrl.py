b87518e07a9f3569bf3f27baf2c90e36d4669650
Ju_M <julienmarc@free.fr>
2025-01-18 13:14:41 +0100

    imprtoved docstrings for login_ctrl

--- controllers/login_ctrl.py
+++ controllers/login_ctrl.py
@@ -8,10 +8,11 @@ from views.view import View
 
 
 class LoginController:
-    """Controller to manage the main login menu"""
+    """Controller to manage the main login menu process"""
 
     @staticmethod
     def run():
+        """Handles the initial login screen user choices"""
         LoginView.firstscreen()
         while True:
             choice = LoginView.login_screen()
@@ -25,6 +26,7 @@ class LoginController:
                     View.input_return_prints("choice_error")
 
     def login():
+        """Manages the login (login data input and authentication processes)"""
         try:
             # Fetch user data
             username, password = LoginView.get_login_data()
@@ -42,19 +44,15 @@ class LoginController:
 
 
 class AuthController:
-    """controller for user authentication
-
-    Raises:
-        ValueError
-
-    Returns:
-        The authenticated user object, or None if authentication fails.
-    """
+    """controller to manage user authentication logic"""
 
     @staticmethod
     @with_session
     def login(username, password, session=None):
-        """Handles user login logic."""
+        """Handles user login logic by validating username and password
+        Returns:
+            User: The authenticated user object, or None if authentication fails.
+        """
         try:
             # Find the user in the database
             user = session.query(User).filter_by(username=username).one()
